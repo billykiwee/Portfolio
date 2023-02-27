741b7b0a4932f85a7f47eaafda1538379efd1379
Billy <billyturpin642@gmail.com>
2023-02-27 14:33:30 +0100

    2023-02-27 14:33:30

--- src/Website/views/ProjectView.js
+++ src/Website/views/ProjectView.js
@@ -6,19 +6,59 @@ export default function ProjectView() {
     return (
         <Container>
 
-            <div className='display justify-s-b w-100p'>
-                <div className='grid'>
+            <div className='display w-100p justify-s-b' style={{ width: '800px' }}>
+                <div className='grid w-100p'>
                     <span className='f-w-600'>TJM</span>
-                    <span className=''>Turpin Jason</span>
-                    <span className=''>34 b chemin des palmistes</span>
-                    <span>Palmiste Rouge</span>
-                    <span className=''>CILOAS 97413</span>
+                    <Adress 
+                        name='Turpin Jason'
+                        adress='34 chemin des palmistes'
+                        additionalAdress='Palmiste rouge'
+                        city='Cilaos'
+                        zipCode='97413'
+                    />
                 </div>
 
-                <div>
-                    <span>FACTURE</span>
+                <div style={{ textAlign: 'end' }} className=''>
+                    <span className='f-w-600 f-s-25'>FACTURE</span>
+                    <Adress 
+                        name='Mr Martial'
+                        adress='34 chemin des palmistes'
+                        city='Cilaos'
+                        zipCode='97413'
+                    />
                 </div>
             </div>
         </Container>
+    )
+}
+
+
+function Adress({ name, adress, additionalAdress, city, zipCode  }) {
+
+    return (
+        <div className='grid w-100p m-t-1' >
+            {
+                name &&
+                <span contentEditable>{name}</span>
+            }
+            {
+                adress &&
+                <span contentEditable>{adress}</span>   
+            }
+            {
+                additionalAdress &&
+                <span contentEditable>{additionalAdress}</span>
+            }
+            <div className='display gap'>
+                {
+                    city &&
+                    <span contentEditable>{city.toUpperCase()}</span> 
+                }
+                {
+                    zipCode &&
+                    <span>{zipCode}</span>
+                }
+            </div>
+        </div>
     )
 }
