d2cf1d223a42ef264bd2cfc041376f1327100926
Billy <billyturpin642@gmail.com>
2023-03-04 12:07:54 +0100

    2023-03-04 12:07:54

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import './App/css/style.css'
 
-
 const element = document.getElementById('root')
 
-
 if (element) {
-    const root = ReactDOM.createRoot(element)
-    
-    root.render(<App />)
+    ReactDOM.createRoot(element).render(<App />)
 }
 
 
