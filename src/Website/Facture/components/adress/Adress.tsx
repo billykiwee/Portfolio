330c1b93545b6cee53903ab8a57fdca4ce49fd4d
Billy <billyturpin642@gmail.com>
2023-03-05 15:34:55 +0100

    2023-03-05 15:34:55

--- src/Website/Facture/components/adress/Adress.tsx
+++ src/Website/Facture/components/adress/Adress.tsx
@@ -22,91 +22,30 @@ export class Adress extends React.Component<AddressProps, EditAdressState> {
         
         return (
             <div className='grid w-100p'  >
-                {
-                    !this.state.editAdress 
-                    ?
-                    <div className="grid">
+                <div className="grid">
+                    {
+                        this.props.name &&
+                        <span contentEditable>{this.props.name}</span>
+                    }
+                    {
+                        this.props.adress &&
+                        <span contentEditable>{this.props.adress}</span>   
+                    }
+                    {
+                        this.props.additionalAdress &&
+                        <span contentEditable>{this.props.additionalAdress}</span>
+                    }
+                    <div>
                         {
-                            this.props.name &&
-                            <span >{this.props.name}</span>
+                            this.props.city &&
+                            <span contentEditable>{this.props.city.toUpperCase()}</span> 
                         }
                         {
-                            this.props.adress &&
-                            <span >{this.props.adress}</span>   
+                            this.props.zipCode &&
+                            <span contentEditable className='m-l-04'>{this.props.zipCode}</span>
                         }
-                        {
-                            this.props.additionalAdress &&
-                            <span >{this.props.additionalAdress}</span>
-                        }
-                        <div>
-                            {
-                                this.props.city &&
-                                <span >{this.props.city.toUpperCase()}</span> 
-                            }
-                            {
-                                this.props.zipCode &&
-                                <span  className='m-l-04'>{this.props.zipCode}</span>
-                            }
-                        </div>
                     </div>
-                    : 
-                    <div className="grid gap-1rem absolute shadow white p-2 zi-2 w-100p border-r-1" id='edit-adress-block'>
-                        <div className="grid gap">
-                            {
-                                this.props.name &&
-                                <div style={{ textAlign: 'start' }}>
-                                    <label>Nom</label>
-                                    <input className="w-100p" placeholder={this.props.name} />
-                                </div>
-                            }
-                            {
-                                this.props.adress &&
-                                <div style={{ textAlign: 'start' }}>
-                                    <label>Adress</label>
-                                    <input className="w-100p" placeholder={this.props.adress} />
-                                </div>
-                            }
-                            {
-                                this.props.additionalAdress &&
-                                <div style={{ textAlign: 'start' }}>
-                                    <label>additionalAdress</label>
-                                    <input className="w-100p" placeholder={this.props.additionalAdress} />
-                                </div>
-                            }
-                            <div className="grid gap">
-                                {
-                                    this.props.city &&
-                                    this.props.additionalAdress &&
-                                    <div style={{ textAlign: 'start' }}>
-                                        <label>Ville</label>
-                                        <input className="w-100p" placeholder={this.props.city} />
-                                    </div>
-                                }
-                                {
-                                    this.props.zipCode &&
-                                    <div style={{ textAlign: 'start' }}>
-                                        <label>Code Postal</label>
-                                        <input className="w-100p" placeholder={this.props.zipCode.toString()} />
-                                    </div>
-                                }
-                            </div>
-                        </div>
-                        <button className="blue h-3">
-                            <span className="f-s-18">Valider</span>
-                        </button>
-                    </div>
-                }
-                
-                {
-                    this.props.clientAdress &&
-                    <div className="display justify-e">
-                        <div className="display">
-                            <button className="red" onClick={e=> this.setState({ editAdress: !this.state.editAdress })} >
-                                <span>Modifier</span>
-                            </button>
-                        </div>
-                    </div>
-                }
+                </div>
             </div>
         )
     }
