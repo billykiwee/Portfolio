b2e45c9fe0636e1b4539abb27d2b5645e70ec9f0
Billy <billyturpin642@gmail.com>
2023-03-05 12:01:36 +0100

    2023-03-05 12:01:36

--- src/Website/views/ProjectView.tsx
+++ src/Website/views/ProjectView.tsx
@@ -18,7 +18,110 @@ interface jsPDFOptions {
     unit            : any,
 }
 
-export default function ProjectView(): JSX.Element {
+interface ProjectViewState {
+    ref: HTMLDivElement | null
+    visible : (visible: boolean) => void 
+}
+type ProjectViewProps = ProjectViewState
+
+export class ProjectView extends React.Component<ProjectViewProps, ProjectViewState> {
+
+    constructor(props: ProjectViewProps) {
+        super(props)
+
+        this.state = {
+            visible: this.props.visible,
+            ref    : null
+        }
+
+
+        this.ref = React.createRef<HTMLDivElement>();
+    }
+
+
+
+    handleGeneratePdf() {
+
+        const options : jsPDFOptions = {
+            format: "a4",
+            unit: "px"
+        }
+        
+        const doc = new jsPDF(options)
+        doc.setFont("Poppins", "normal");
+        setvisible(false)
+
+        if (reportTemplateRef.current) {
+            doc.html(reportTemplateRef.current, {
+                async callback(doc) {
+                    doc.save('Facture n° ' + ID)
+                    setvisible(true)
+                }
+            })
+        }
+	}
+
+
+    render() {
+        return (
+
+            <div className='display gap  p-2 h-100p' style={{ width: 'calc(500px)', alignItems: 'baseline' }} ref={reportTemplateRef} >
+                <div className='grid m-2' >
+                    <div className='display w-100p justify-s-b'>
+                        <div className='grid w-100p'>
+                            <span className='f-w-600'>
+                               <img src='./images/tjm.png' height={44} /> 
+                            </span>
+                            <Adress 
+                                name='Turpin Jason'
+                                adress='34 chemin des palmistes'
+                                additionalAdress='Palmiste rouge'
+                                city='Cilaos'
+                                zipCode={97413}
+                            />
+                        </div>
+    
+                        <div style={{ textAlign: 'end' }} className='w-100p'>
+                            <span className='f-w-600 f-s-20'>FACTURE</span>
+                            <Adress 
+                                name='Mr Martial'
+                                adress='34 chemin des palmistes'
+                                city='Cilaos'
+                                additionalAdress=''
+                                zipCode={97413}
+                            />
+                        </div>
+                    </div>
+    
+    
+                    <div className='m-t-4 display justify-s-b align-top'>
+                        <Project projet='Fabrication' />
+                        <Encode />
+                    </div>
+    
+                    <Table visible={visible} />   
+                
+    
+                    <div className='display m-t-1' style={{ bottom: 0 }}>
+                        <BottomPage />
+                    </div>
+                </div>
+                {
+                    visible&&
+                    <div className='display justify-c fixed p-1' style={{ bottom : 0 }}>
+                        <div className='display'>
+                            <button className='blue p-1' onClick={handleGeneratePdf}>
+                                <span className='f-s-20'>Télécharger en PDF</span>
+                            </button>
+                        </div>
+                    </div>
+                }
+            </div>
+        )
+    }
+}
+
+/* export default function ProjectView(): JSX.Element {
 
     const [visible, setvisible] = useState<boolean>(true)
 
@@ -44,8 +147,6 @@ export default function ProjectView(): JSX.Element {
             })
         }
 	}
-
-
 
     return (
 
@@ -103,7 +204,7 @@ export default function ProjectView(): JSX.Element {
         </div>
     )
 }
-
+ */
 interface TableProps {
     visible : boolean
 } 
