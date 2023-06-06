542d5e61615a9a144905687aac8f73e5bc0ffc68
Billy <99732900+billykiwee@users.noreply.github.com>
2023-06-06 18:38:38 +0200

    Update Bottom.tsx

--- src/Website/Facture/components/bottom/Bottom.tsx
+++ src/Website/Facture/components/bottom/Bottom.tsx
@@ -8,7 +8,7 @@ export class BottomPage extends React.Component {
                 <div className='grid' style={{ textAlign: 'center', fontSize: '12px' }} >
                     <span>TVA non applicable, art. 293 B du CGI.</span>
                     <span>TJM - MR TURPIN PHILIPPE JASON</span>
-                    <span>Numéro de SIRET : 8892147800014</span>
+                    <span>Numéro de SIRET : 95258153600010</span>
                     <span>Adresse : 34 b chemin des palmistes, Palmiste Rouge, CILAOS 97413</span>
                     <span>Téléphone : 06 92 35 80 12</span>
                 </div>
